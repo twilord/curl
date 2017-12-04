0ec5b815ddea975d7cd7d738b82d4383d298359f
wzy <m15098751939@163.com>
2017-12-04 23:36:38 +0800

    getarticial

--- curl.php
+++ curl.php
@@ -1,9 +1,19 @@
 <?php
+	function getartical($output)
+	{
+		$reg_tag_a = '/http://[a-zA-Z0-9./-_]+.html|php|htm/';
+		$result = preg_match_all($reg_tag_a,$output,$match_result);
+		if($result)
+		{
+			print_r($match_result);
+		}
+	}
+
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "www.sdu.edu.cn");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	$output = curl_exec($ch);
 	curl_close($ch);
-	print_r($output);
+	getartical($output);
 ?>
